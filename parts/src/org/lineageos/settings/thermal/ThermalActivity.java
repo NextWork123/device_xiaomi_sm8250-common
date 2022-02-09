d73801afd2a9f36171a6eae1cd6390dd25dbc232
NextWorks <46625863+NextWork123@users.noreply.github.com>
2022-02-09 18:23:28 +0100

    Revert "sm8250-common: Adapt Thermal Profiles to Android S"
    This reverts commit b0c7651d0be607fc6be5319956a287c616dbee53.

--- parts/src/org/lineageos/settings/thermal/ThermalActivity.java
+++ parts/src/org/lineageos/settings/thermal/ThermalActivity.java
@@ -17,14 +17,11 @@
 package org.lineageos.settings.thermal;
 
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.preference.PreferenceActivity;
 
 import org.lineageos.settings.utils.FileUtils;
 
-import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
-
-public class ThermalActivity extends CollapsingToolbarBaseActivity {
+public class ThermalActivity extends PreferenceActivity {
 
     private static final String TAG_THERMAL = "thermal";
     private static final String THERMAL_SCONFIG = "/sys/class/thermal/thermal_message/sconfig";
@@ -34,16 +31,8 @@ public class ThermalActivity extends CollapsingToolbarBaseActivity {
         super.onCreate(savedInstanceState);
 
         if (FileUtils.fileExists(THERMAL_SCONFIG)) {
-		getFragmentManager().beginTransaction().replace(R.id.content_frame,
+            getFragmentManager().beginTransaction().replace(android.R.id.content,
                     new ThermalSettingsFragment(), TAG_THERMAL).commit();
         }
-    }
-     @Override
-     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-             onBackPressed();
-             return true;
-        }
-	return false;
     }
 }
